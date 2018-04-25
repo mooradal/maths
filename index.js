3583d71c619442d60faded263d37796049b1e73d
Moorad <34781684+mooradal@users.noreply.github.com>
2018-04-25 10:32:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-input = document.getElementById("input")
+question = document.getElementById("question")
+input = document.getElementById("input")  
+
+function main() {
+  window.requestAnimationFrame(main)
+  console.log(input.value)
+  Add()
+}
+
+function Add(){
+ var Num1 = Mtah.floor(Math.random() * 100)
+ var Num2 = Math.floor(Math.random() * 100)
+ question.innerHTML = Num1 + " + " + Num2
+ if
+}
+document.onkeyup = function(event) {
+    var key_up = event.keyCode;
+  if (keyup == 13)
+};
