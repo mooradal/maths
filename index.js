0e01e8e911ecbac46289a43472fa80234df6a0ae
Moorad <34781684+mooradal@users.noreply.github.com>
2018-04-26 07:50:46 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
-question = document.getElementById("question")
-input = document.getElementById("input")  
+var question = document.getElementById("question")
+var input = document.getElementById("input")  
+var enter = false;
+var next = true;
 
+main()
 function main() {
   window.requestAnimationFrame(main)
-  console.log(input.value)
   Add()
 }
 
 function Add(){
- var Num1 = Mtah.floor(Math.random() * 100)
- var Num2 = Math.floor(Math.random() * 100)
- question.innerHTML = Num1 + " + " + Num2
- if
+	if (next == true) {
+ 		 Num1 = Math.floor(Math.random() * 100)
+ 		 Num2 = Math.floor(Math.random() * 100)
+
+ 		question.innerHTML = Num1 + " + " + Num2
+ 		next = false;
+	}	
+ if (enter == true) {
+ 	enter = false;
+ 	if (Num1 + Num2 == input.value) {
+ 		console.log("right answer")
+ 		input.style.borderColor = "#00FF00"
+ 	} else {
+ 		console.log("Wrong answer")
+ 		input.style.borderColor = "#FF0000"
+
+ 	}
+ 	next = true
+ 	}
 }
+
 document.onkeyup = function(event) {
-    var key_up = event.keyCode;
-  if (keyup == 13)
+  key_up = event.keyCode;
+  if (key_up == 13) {
+  enter = true
+  }
 };
