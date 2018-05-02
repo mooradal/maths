9e065737a23c540f051e7498b7faa01cbb2ef330
Moorad <34781684+mooradal@users.noreply.github.com>
2018-05-02 07:52:41 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 var question = document.getElementById("question")
 var input = document.getElementById("input")  
+var select = document.getElementById("select")
 var enter = false;
 var next = true;
+var oldvalue = select.value
+var correct = document.getElementById("correct");
 
 main()
 function main() {
   window.requestAnimationFrame(main)
-  Add()
+  console.log(select.value)
+  if (select.value != oldvalue) {
+  	oldvalue = select.value
+  	next = true
+  }
+  if (select.value == "addition") {
+ 	Add()
+  } else if (select.value == "subtraction") {
+  	Sub()
+  }
+}
+
+function createNum(range) {
+	Num1 = Math.floor(Math.random() * range)
+ 	Num2 = Math.floor(Math.random() * range)
 }
 
 function Add(){
 	if (next == true) {
- 		 Num1 = Math.floor(Math.random() * 100)
- 		 Num2 = Math.floor(Math.random() * 100)
+		createNum(100)
 
  		question.innerHTML = Num1 + " + " + Num2
  		next = false;
@@ -20,14 +36,36 @@ function Add(){
  if (enter == true) {
  	enter = false;
  	if (Num1 + Num2 == input.value) {
- 		console.log("right answer")
  		input.style.borderColor = "#00FF00"
+ 		correct.innerHTML = ""
  	} else {
- 		console.log("Wrong answer")
  		input.style.borderColor = "#FF0000"
+ 		correct.innerHTML = Num1 + Num2
+ 	}
+ 	input.value = ""
+ 	next = true
+ 	}
+}
 
+function Sub(){
+	if (next == true) {
+		createNum(100)
+
+ 		question.innerHTML = Num1 + " - " + Num2
+ 		next = false;
+	}	
+ if (enter == true) {
+ 	enter = false;
+ 	if (Num1 - Num2 == input.value) {
+ 		input.style.borderColor = "#00FF00"
+ 		correct.innerHTML = ""
+ 	} else {
+ 		console.log(Num1 + " " + Num2)
+ 		input.style.borderColor = "#FF0000"
+ 		correct.innerHTML = Num1 - Num2
  	}
  	next = true
+ 	input.value = ""
  	}
 }
 
